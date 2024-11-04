c78223378257e9b84432b61524cc9a4737338113
David Elizalde <david.elizalde.r.a@gmail.com>
2024-11-03 19:08:57 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Map from './components/Map.vue';
+import 'mapbox-gl/dist/mapbox-gl.css'; // Correct import path for Mapbox CSS
+</script>
+
+<template>
+  <div id="layout">
+    <Map />
+  </div>
+</template>
+
+<style>
+#layout {
+  display: flex;
+  flex: 1;
+  position: relative;
+  height: 100vh; /* Ensures full height for the map container */
+  width: 100vw; /* Ensures full width for the map container */
+}
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,36 @@
+<template>
+  <div ref="mapContainer" class="map-container"></div>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'REPLACENME';
+
+export default {
+  mounted() {
+    this.map = new mapboxgl.Map({
+      container: this.$refs.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [-71.224518, 42.213995],
+      zoom: 9,
+      pitch: 60, // 3D effect
+      bearing: 0
+    });
+  },
+
+  unmounted() {
+    if (this.map) {
+      this.map.remove();
+      this.map = null;
+    }
+  }
+};
+</script>
+
+<style>
+.map-container {
+  width: 100%;
+  height: 100vh; /* Ensure the map takes up full screen height */
+}
+</style>
